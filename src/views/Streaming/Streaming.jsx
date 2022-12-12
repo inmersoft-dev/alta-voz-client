bfb9f0dfd10d047078ca5de48062c9975415126a
Sito8943 <sito8943@gmail.com>
2022-12-12 17:14:27 +0100

    doing recomendations

--- src/views/Streaming/Streaming.jsx
+++ src/views/Streaming/Streaming.jsx
@@ -8,6 +8,7 @@ import Albums from "./Albums/Albums";
 import NewAlbums from "./NewAlbums/NewAlbums";
 import NewSimples from "./NewSimples/NewSimples";
 import TopCharts from "./TopCharts/TopCharts";
+import Recommendations from "./Recommendations/Recommendations";
 // import CatchToAction from "./CatchToAction/CatchToAction";
 
 const Streaming = () => {
@@ -19,6 +20,7 @@ const Streaming = () => {
       <NewAlbums />
       <NewSimples />
       <TopCharts />
+      <Recommendations />
       {/* <CatchToAction /> */}
     </SitoContainer>
   );
